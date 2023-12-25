a86f21701a27ba3251ef0d2401bb5a9287f67162
John Nguyen <jnguyen92780@gmail.com>
2023-12-24 17:46:03 -0800

    Created route to index

---
+++ app/routes/_index.tsx
@@ -0,0 +1,17 @@
+export function meta() {
+  return [
+    {title: 'JohnnyFiive'},
+    {
+      description:
+        'My second website. I am a big shot developer now. EVEN MORE MONEY PLS.',
+    },
+  ];
+}
+
+export default function Index() {
+  return (
+    <div>
+      <h1>JohnnyFiive</h1>
+    </div>
+  );
+}
